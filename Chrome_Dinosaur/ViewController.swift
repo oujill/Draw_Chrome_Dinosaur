c1670a1e93a69b8537c3bb725953805292b0b105
jillou <jilljill860524@gmail.com>
2020-07-12 21:07:25 +0800

    Initial Commit

---
+++ Chrome_Dinosaur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chrome_Dinosaur
+//
+//  Created by JillOU on 2020/7/12.
+//  Copyright © 2020 Jillou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
